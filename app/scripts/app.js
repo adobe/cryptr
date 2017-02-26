eb07118ba6e07c2698f63546f71c4fa48579ea8d
Jacob Crowther <jacobcrowther@gmail.com>
2017-02-25 23:39:58 -0700

    - Create secrets - Refresh secrets - Cleanup unused "all" page - Rename initialResponse to loginResponse for better standardization - Add temporary Create page (until better location determined) - Fix access parsing bug in secrets-init - Increase login timeout to 30 seconds to allow for Two Factor Auth notification confirmation - Fix bug where secrets not cleared from memory on logout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,14 +3,6 @@
 	var app = document.querySelector('#app');
 	app.baseUrl = '/';
 
-	app.addEventListener('dom-change', function() {
-		// console.log('App ready');
-	});
-
-	window.addEventListener('WebComponentsReady', function() {
-		// imports are loaded and elements have been registered
-	});
-
 	app.closeDrawer = function() {
 		app.$.paperDrawerPanel.closeDrawer();
 	};
